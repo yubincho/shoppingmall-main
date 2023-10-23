8e77031ee21ea38ab6f4a11423b12834f9e0901a
yubin <yubincho9@gmail.com>
2023-10-23 12:43:15 +0900

    add transaction

--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -5,6 +5,7 @@ import { DataSource, Repository } from 'typeorm';
 import { IPointTransactionCreate } from './interfaces/IPointTransactionCreate';
 import { POINT_TRANSACTION_STATUS_ENUM } from './entities/point-transaction-status.enum';
 import { Member } from '../member/entities/member.entity';
+import * as querystring from 'querystring';
 
 @Injectable()
 export class OrderService {
@@ -18,28 +19,44 @@ export class OrderService {
   ) {}
 
   async create({ impUid, amount, user: _user }: IPointTransactionCreate) {
-    // order 테이블에 거래기록 1줄 생성
+    const queryRunner = this.dataSource.createQueryRunner();
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
 
-    const pointTransaction = {
-      impUid,
-      orderAmount: amount,
-      user: _user,
-      status: POINT_TRANSACTION_STATUS_ENUM.PAYMENT,
-    };
-    const createTransaction = this.orderRepository.create(pointTransaction);
-    await this.orderRepository.save(createTransaction);
+    try {
+      // order 테이블에 거래기록 1줄 생성
+      const pointTransaction = {
+        impUid,
+        orderAmount: amount,
+        user: _user,
+        status: POINT_TRANSACTION_STATUS_ENUM.PAYMENT,
+      };
+      const createTransaction = this.orderRepository.create(pointTransaction);
+      await queryRunner.manager.save(createTransaction);
 
-    console.log('_user.id', _user.id);
-    // user 의 돈 찾아오기
-    const user = await this.memberRepository.findOne({
-      where: { id: _user.id },
-    });
-    // user의 돈 업데이트 -> save 쓰면 쿼리 2번 나가서 비효율적
-    await this.memberRepository.update(
-      { id: _user.id },
-      { point: user.point + amount },
-    );
-    // 최종결과 브라우저에 돌려주기
-    return pointTransaction;
+      // throw new Error('예기치 못한 실패!!'); // rollback 되는지 테스트
+
+      console.log('_user.id', _user.id);
+      // user 의 돈 찾아오기
+      const user = await queryRunner.manager.findOne(Member, {
+        where: { id: _user.id },
+      });
+
+      // user의 돈 업데이트 -> save 쓰면 쿼리 2번 나가서 비효율적
+      const updatedUser = await this.memberRepository.create({
+        ...user,
+        point: user.point + amount,
+      });
+      await queryRunner.manager.save(updatedUser);
+
+      await queryRunner.commitTransaction();
+
+      // 최종결과 브라우저에 돌려주기
+      return pointTransaction;
+    } catch (error) {
+      await queryRunner.rollbackTransaction();
+    } finally {
+      await queryRunner.release();
+    }
   }
 }
